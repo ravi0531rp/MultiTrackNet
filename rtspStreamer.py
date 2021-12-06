248cb38eceb9b89d1b4cedc59ed54ebb86271caa
ravi0531rp <ravi.0531.rp@gmail.com>
2021-12-06 23:15:13 +0530

    rtspInit

---
+++ rtspStreamer.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from threading import Thread
+
+class Rtsp(object):
+    def __init__(self, src = None, camId = None, shouldLoop = False, sampleRate = 5 ):
+        self.src = src
+        self.camId = camId
+        self.shouldLoop = shouldLoop
+        self.sampleRate = sampleRate
+        self.endStream = False
+
+    def start(self):
+        Thread(target=self.streamRtsp).start()
+
+    def stop(self):
+        self.endStream = True
+    def streamRtsp(self):
+        cap = cv2.VideoCapture(self.src)
+        if self.shouldLoop:
+            while True:
+                for _ in range(self.sampleRate):
+                    ret, frame = cap.read()
+                if ret:
+                    print("Da da da...")
+    
+
+
+
